ba416ad5beb35d9cc69f15f6c9d25281ed4cee2c
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2021-10-16 13:47:30 +0300

    added searching in database

--- contacts/contacttable/api/contact.py
+++ contacts/contacttable/api/contact.py
@@ -1,20 +1,46 @@
-from django.http import HttpResponse
-from django.views.generic import View
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from contacttable.models import Contacts
+from rest_framework.views import APIView
+from django.forms.models import model_to_dict
 
-class ContactApi(View):
+class ContactApi(APIView):
     @csrf_exempt
-    def put(self, request, id, *args, **kwargs):
-        c1 = Contacts.objects.get(pk=id)
-        c1.Name = "andrew"
-        c1.Phone = "0007"
-        c1.Surname = "121"
-        c1.save()
-        return HttpResponse('success')
+    def put(self, request, id):
+        try:
+            c1 = Contacts.objects.get(pk=id)
+
+            name = request.data.get("name", c1.Name)
+            surname = request.data.get("surname", c1.Surname)
+            phone = request.data.get("phone", c1.Phone)
+            address = request.data.get("address", c1.Address)
+            country = request.data.get("country", c1.Country)
+            town = request.data.get("town", c1.Town)
+            street = request.data.get("street", c1.Street)
+            url = request.data.get("url", c1.Url)
+            photo = request.data.get("photo", c1.Photo)
+
+            c1.Name = name
+            c1.Phone = phone
+            c1.Surname = surname
+            c1.Address = address
+            c1.Country = country
+            c1.Town = town
+            c1.Street = street
+            c1.Url = url
+            c1.Photo = photo
+
+            c1.save()
+        except Contacts.DoesNotExist:
+            return HttpResponse('Такого ID не существует', status=404)
+
+        return JsonResponse(model_to_dict(c1))
 
     @csrf_exempt
-    def delete(self, request, *args, **kwargs):
-        wd = Contacts.objects.filter(pk=1)
-        wd.delete()
-        return HttpResponse('success')
+    def delete(self, request, id):
+        try:
+            wd = Contacts.objects.get(pk=id)
+            wd.delete()
+            return JsonResponse(model_to_dict(wd))
+        except Contacts.DoesNotExist:
+            return HttpResponse('Такого ID не существует', status=404)
--- contacts/contacttable/api/contacts.py
+++ contacts/contacttable/api/contacts.py
@@ -1,15 +1,38 @@
-from django.http import HttpResponse
-from django.views.generic import View
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from contacttable.models import Contacts
+from rest_framework.views import APIView
+from django.db.models import Q
+from django.forms.models import model_to_dict
 
 
-class ContactsApi(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse(Contacts)
+class ContactsApi(APIView):
+    def get(self, request):
+        search = request.query_params.get('search')
+
+        if search is not None:
+            c1 = Contacts.objects.filter(Q(Name=search) | Q(Surname=search))
+        else:
+            c1 = Contacts.objects.all()
+
+        return JsonResponse(list(c1.values()), safe=False)
 
     @csrf_exempt
-    def post(self, request, *args, **kwargs):
-        c1 = Contacts(Name="ihor", Surname="0006", Phone="222")
+    def post(self, request):
+        name = request.data.get("name")
+        surname = request.data.get("surname")
+        phone = request.data.get("phone")
+        address = request.data.get("address", "")
+        country = request.data.get("country", "")
+        town = request.data.get("town", "")
+        street = request.data.get("street", "")
+        url = request.data.get("url", "")
+        photo = request.data.get("photo", "")
+
+        if name is None or surname is None or phone is None:
+            return HttpResponse("Вы не ввели достаточно информации, повторите попытку", status=400)
+        else:
+            c1 = Contacts(Name=name, Surname=surname, Phone=phone, Address=address,
+                          Country=country, Town=town, Street=street, Url=url, Photo=photo)
         c1.save()
-        return HttpResponse("success")
+        return JsonResponse(model_to_dict(c1), status=201)
